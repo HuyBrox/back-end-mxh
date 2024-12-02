f0f961245f8b5206386c69461aa1c36597de3bc5
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2024-12-02 20:23:28 +0700

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -165,24 +165,28 @@ export const login = async (req, res) => {
         //Tạo token - tham số đầu tiên là payload, tham số thứ 2 là secret key
         const token = await jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
         console.log(token);
-        // return res.cookie("token", token, {
-        //     httpOnly: true,     //cookie không thể được truy cập bằng mã JavaScript
-        //     // secure: true,       //cookie chỉ được gửi khi có https
-        //     sameSite: "strict", //cookie chỉ được gửi khi có cùng domain
-        //     maxAge: 1 * 10 * 60 * 60 * 1000, //5h
-        // }).json({
-        //     message: "Đăng nhập thành công",
-        //     success: true,
-        //     user,
-        // });
-        return res.status(200).json({
-            message: "Thành công",
+        return res.cookie("token", token, {
+            httpOnly: true,     //cookie không thể được truy cập bằng mã JavaScript
+            secure: true,       //cookie chỉ được gửi khi có https
+            sameSite: "none",  //cookie chỉ được gửi khi có cùng domain
+            maxAge: 1 * 10 * 60 * 60 * 1000, //5h
+        }).json({
+            message: "Đăng nhập thành công",
             success: true,
-            token,
             user,
         });
+        // return res.status(200).json({
+        //     message: "Thành công",
+        //     success: true,
+        //     token,
+        //     user,
+        // });
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            message: "Có lỗi xảy ra, vui lòng thử lại sau",
+            success: false,
+        });
     }
 }
 //[GET] /api/user/logout
